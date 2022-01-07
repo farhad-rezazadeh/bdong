ccdc36df1e9bac620b2bc9c742de33e71ab730fa
farhad <alireza.rezazadeh.official@gmail.com>
2022-01-07 14:29:41 +0330

    fix lock file

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /code
 # Install dependencies
 COPY Pipfile Pipfile.lock /code/
-RUN pip install pipenv && pipenv install --system --skip-lock
+RUN pip install pipenv && pipenv install --system
 # Copy project
 COPY . /code/
--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xutmtqv4bokqwv09s0o#y=k#z*soz^n_2mh7wj=_8w=tszcf+o"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -158,7 +158,7 @@ EMAIL_PORT = os.getenv("EMAIL_PORT")
 LOGIN_URL = "account:login"
 
 LOGIN_REDIRECT_URL = "dashboard:dashboard"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "account:login"
 
 # Google Login
 AUTHENTICATION_BACKENDS = (
